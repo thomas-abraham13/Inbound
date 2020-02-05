0e66c00cb39fb99d844cab13d154f5b4209b12c5
Thomas Abraham <thomasabraham832@gmail.com>
2020-02-05 12:56:14 +0530

    fully functional basic signup and login page added

--- app/src/main/java/com/example/inboundlogin/Inbound_login.java
+++ app/src/main/java/com/example/inboundlogin/Inbound_login.java
@@ -1,22 +1,42 @@
 package com.example.inboundlogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Inbound_login extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+
+    DatabaseReference databaseUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inbound_login);
 
-        Button next = (Button) findViewById(R.id.button3);
-        next.setOnClickListener((view) -> {
-            Intent i = new Intent(this, TeamRegistration.class);
-            startActivity(i);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        databaseUsers= FirebaseDatabase.getInstance().getReference("user");
+
+        Button login = (Button) findViewById(R.id.button3);
+        login.setOnClickListener((view) -> {
+
+            Authenticate(username.getText().toString(),password.getText().toString());
+
         });
 
         Button cancel = (Button) findViewById(R.id.button4);
@@ -26,4 +46,29 @@ public class Inbound_login extends AppCompatActivity {
         });
 
     }
+
+    private void Authenticate(final String uname, final String upassword) {
+        databaseUsers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot userSnapshot: dataSnapshot.getChildren()){
+                    Users user = userSnapshot.getValue(Users.class);
+
+                    if(uname.equals(user.getEmailid())&& upassword.equals(user.getPass())){
+                        Intent i = new Intent(Inbound_login.this, TeamRegistration.class);
+                        startActivity(i);
+                    }
+                    else{
+                        Toast.makeText(Inbound_login.this, "Username or Password is Incorrect", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
